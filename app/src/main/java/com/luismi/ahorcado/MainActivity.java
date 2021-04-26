e17c06b207193b2fd70165f8343c33f7221b8e3c
luiskawaiineko <55877411+luiskawaiineko@users.noreply.github.com>
2021-04-26 13:01:44 +0200

    c
    c

--- app/src/main/java/com/luismi/ahorcado/MainActivity.java
+++ app/src/main/java/com/luismi/ahorcado/MainActivity.java
@@ -8,25 +8,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button signUpButton = findViewById(R.id.signUpButton);
-        final Button signInButton = findViewById(R.id.signUpButton);
-        //registro
-        signUpButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
-        //inicio de sesión
-        signInButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-
-            }
-        });
+    }
+    public void signUp(View view){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/luismi/ahorcado/SignUpActivity.java
+++ app/src/main/java/com/luismi/ahorcado/SignUpActivity.java
@@ -1,14 +1,69 @@
 package com.luismi.ahorcado;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "EmailPassword";
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void reload() { }
+
+
+    public void register(){
+        mAuth.createUserWithEmailAndPassword(((EditText)findViewById(R.id.inputMail)).getText().toString(), ((EditText)findViewById(R.id.inputPass)).getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                            Toast.makeText(SignUpActivity.this, "EXITOOOOOO",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+
+    }
+
+    private void updateUI(FirebaseUser user) {
+
     }
 }
