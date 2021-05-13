3693a204514cd92e0b8eeadd3c4a99b9ee3df18a
luiskawaiineko <55877411+luiskawaiineko@users.noreply.github.com>
2021-05-13 13:01:59 +0200

    a
    a

--- app/src/main/java/com/luismi/ahorcado/GameActivity.java
+++ app/src/main/java/com/luismi/ahorcado/GameActivity.java
@@ -1,7 +1,10 @@
 package com.luismi.ahorcado;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,10 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         TextView showSalaCode = (TextView) findViewById(R.id.codigoSala);
+        ImageView man = (ImageView) findViewById(R.id.manView);
+        Bitmap bmpSource = BitmapFactory.decodeResource(getResources(), SourceFileId);
+        Bitmap bmpScaled = Bitmap.createScaledBitmap(bmpSource, 100, 100, false);
+        man.setImageBitmap(bmpScaled);
         showSalaCode.setText(idSala);
     }
     public GameActivity() {
